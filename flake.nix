fd50c6f04689d173080c084c98f39a1e14e9b0ee
robotnik-dev <robotnik.games@gmail.com>
2025-10-18 18:20:11 +0200

    refactor: to work with flakes and dioxus-cli v0.6

---
+++ flake.nix
@@ -0,0 +1,118 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+
+      perSystem =
+        {
+          self',
+          config,
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+            ];
+            targets = [ "wasm32-unknown-unknown" ];
+          };
+          rustBuildInputs =
+            (with pkgs; [
+              openssl
+              libiconv
+              pkg-config
+            ])
+            ++ lib.optionals pkgs.stdenv.isLinux (
+              with pkgs;
+              [
+                glib
+                gtk3
+                libsoup_3
+                webkitgtk_4_1
+                xdotool
+              ]
+            )
+            ++ lib.optionals pkgs.stdenv.isDarwin (
+              with pkgs.darwin.apple_sdk.frameworks;
+              [
+                SystemConfiguration
+                IOKit
+                Carbon
+                WebKit
+                Security
+                Cocoa
+              ]
+            );
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+          packages.default =
+            let
+              cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+              rev = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
+            in
+            pkgs.rustPlatform.buildRustPackage {
+              pname = cargoToml.package.name;
+              version = "${cargoToml.package.version}-${rev}";
+              src = ./.;
+              strictDeps = true;
+              buildInputs = rustBuildInputs;
+              nativeBuildInputs =
+                with pkgs;
+                [
+                  dioxus-cli
+                  rustToolchain
+                  rustPlatform.bindgenHook
+                  wasm-bindgen-cli_0_2_104
+                ]
+                ++ rustBuildInputs;
+              buildPhase = ''
+                dx build --release --platform web
+              '';
+              installPhase = ''
+                mkdir -p $out
+                cp -r target/dx/$pname/release/web $out/bin
+              '';
+              cargoLock.lockFile = ./Cargo.lock;
+              meta.mainProgram = "server";
+            };
+
+          devShells.default = pkgs.mkShell {
+            name = "dioxus-dev";
+            buildInputs = rustBuildInputs;
+            nativeBuildInputs = with pkgs; [
+              # Add shell dependencies here
+              rustToolchain
+              wasm-bindgen-cli_0_2_104
+              dioxus-cli
+            ];
+            shellHook = ''
+              # For rust-analyzer 'hover' tooltips to work.
+              export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
+            '';
+          };
+        };
+    };
+}
--- shell.nix
+++ shell.nix
@@ -1,26 +1,55 @@
-let
-  pkgs = import <nixpkgs> { };
-in
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    gobject-introspection
-    cargo
-  ];
-
-  buildInputs = with pkgs; [
-    at-spi2-atk
-    atkmm
-    cairo
-    gdk-pixbuf
-    glib
-    gtk3
-    harfbuzz
-    librsvg
-    libsoup_3
-    pango
-    webkitgtk_4_1
-    openssl
-  ];
-  # shellHook = "";
-}
+# {
+#   pkgs ? import <nixpkgs> { },
+# }:
+# let
+#   overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+# in
+# pkgs.callPackage (
+#   {
+#     atkmm,
+#     cairo,
+#     gcc,
+#     gdk-pixbuf,
+#     glib,
+#     gtk3,
+#     mkShell,
+#     openssl,
+#     pango,
+#     pkg-config,
+#     rustup,
+#     rustPlatform,
+#     stdenv,
+#     webkitgtk_4_1, # for javascriptcoregtk-rs-sys
+#     xdotool, # for libxdo
+#   }:
+#   mkShell {
+#     strictDeps = true;
+#     nativeBuildInputs = [
+#       gcc
+#       openssl
+#       pkg-config
+#       rustup
+#       rustPlatform.bindgenHook
+#     ];
+#     # libraries here
+#     buildInputs = [
+#       atkmm
+#       cairo
+#       gdk-pixbuf
+#       glib
+#       gtk3
+#       pango
+#       webkitgtk_4_1
+#       xdotool
+#     ];
+#     GDK_BACKEND = "x11"; # NVIDIA might disagree otherwise.
+#     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+#     RUSTC_VERSION = overrides.toolchain.channel;
+#     WEBKIT_DISABLE_DMABUF_RENDERER = 1; # Again NVIDIA things.
+#     # https://github.com/rust-lang/rust-bindgen#environment-variables
+#     shellHook = ''
+#       export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH"
+#       export PATH="''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin":"$PATH"
+#     '';
+#   }
+# ) { }
