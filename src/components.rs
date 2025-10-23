6c59e4d3d4e3b7c697c84f617e84019d34d31733
robotnik-dev <robotnik.games@gmail.com>
2025-10-23 19:36:42 +0200

    apply fmt

--- src/components.rs
+++ src/components.rs
@@ -1,15 +1,15 @@
 mod card_container;
 mod card_view;
 mod dialog;
+mod history;
 mod nav_bar;
 mod search_bar;
 mod search_view;
-mod history;
 
 pub use card_container::*;
 pub use card_view::*;
 pub use dialog::*;
+pub use history::*;
 pub use nav_bar::*;
 pub use search_bar::*;
 pub use search_view::*;
-pub use history::*;
